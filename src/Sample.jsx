d7de112ef8b4c6f6525d7a224ae3b30a4193a49a
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-13 23:19:28 +0530

    adding debounced search

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
 
+function debounce (func,delay){
+  let timerId;
+  return function(...args){
+    const context = this;
+    clearTimeout(timerId);
+    timerId = setTimeout(()=>{
+      func.apply(context,args);
+    },delay);
+  }
+}
+
 export function Sample() {
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilterdUsers] = useState([]);
   const [searchString, setSearchString] = useState("");
-
+console.log(searchString)
   const fetchData = async () => {
     try {
       const response = await fetch(
@@ -28,6 +39,11 @@ export function Sample() {
     setFilterdUsers(!!searchString ? filtered : users);
   };
 
+  const handleSearchString = (e)=>{
+    const inputValue = e.target.value;
+    debounce(setSearchString(inputValue,500))
+  }
+
   useEffect(() => {
     fetchData();
   },[]);
@@ -40,7 +56,7 @@ export function Sample() {
     <>
       <input id="searchinput"
         value={searchString}
-        onChange={(e) => setSearchString(e.target.value)}
+        onChange={handleSearchString}
       />
       {searchString && (
         <p>
@@ -72,4 +88,24 @@ export function Sample() {
       </table>
     </>
   );
+}
+
+
+
+function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // Set a timer to update debouncedValue after the specified delay
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Clear the timeout if the value changes or the component unmounts
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]); // Re-run effect if value or delay changes
+
+  return debouncedValue;
 }
