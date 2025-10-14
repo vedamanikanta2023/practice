05ab6cdef55060c94122ca68360e931b5ba99003
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-14 18:20:04 +0530

    updating meta data manually

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import { UserDetails } from './UserDetails'
 import { Sample } from './Sample'
 import Alert from './Alert'
+import { MetaData } from './MetaData'
 
 function App() {
   return (
     <>
+    <MetaData />
       <Alert type="success" message={"Your payment is successful"} />
       {/* <UserDetails />    */}
       <Sample /> 
