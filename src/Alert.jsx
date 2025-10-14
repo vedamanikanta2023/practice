967664134f775c1a5105190caf2db074f5733000
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-14 18:21:15 +0530

    added intervel

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
+
 const Alert = ({ type, message }) => {
   const colors = {
     success: "green",
     error: "red",
     warning: "orange",
   };
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer(prevTimer => prevTimer + 1); // Use functional update for correct state
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []); 
+
   return (
     <div style={{ color: colors[type] || "black", padding: "8px" }}>
       {message}
+      <br/>
+      {timer}
     </div>
   );
 };
