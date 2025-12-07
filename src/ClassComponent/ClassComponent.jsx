5950a5c1d64f3392217a1c842b7978db5f9c5f87
vedamanikanta <manikanta.vedam@gmail.com>
2025-12-07 23:55:04 +0530

    props intialized in the class component

--- src/ClassComponent/ClassComponent.jsx
+++ src/ClassComponent/ClassComponent.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   state = { count: "vedam" };
   render() {
-    return <div>{this.state.count}</div>;
+    return (
+      <div>
+        {this.state.count}
+        <button onClick={this.props.onClick}></button>
+      </div>
+    );
   }
 }
 
 export function ClassComponent() {
   const [count, setCount] = React.useState(0);
+  const onClick = () => {
+    setCount((prev) => prev + 1);
+  };
   return (
     <>
       <div>{count}</div>
-      <MyComponent />
+      <MyComponent onClick={onClick} />
     </>
   );
 }
